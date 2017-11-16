5f5d9a6ae88b9fe441bea3727ea9473910ecfc0b
Kate Schlunz <schlunzk@gmail.com>
2017-11-16 18:41:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solution in this file
 function findMatching(drivers, name){
-  drivers.filter(function(driver){
+  return drivers.filter(function(driver){
     if(name === driver){
       return true
     }else{
       return false
     }
 
-    
+
   })
 }
